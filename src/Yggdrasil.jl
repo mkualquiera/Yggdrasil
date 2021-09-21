d20f47be9e56c5b472c569002bf1cf1e505660a1
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2021-09-20 22:45:37 -0500

    Implemented ygg macro

--- src/Yggdrasil.jl
+++ src/Yggdrasil.jl
@@ -1,7 +1,66 @@
 module Yggdrasil
 
-macro set(node,key,value)
-    esc(:($node = set($node, $key, $value)))
+function reftreetovec(tree::Expr)::Vector{Any}
+    if typeof(tree.args[1]) == Expr
+        inner = reftreetovec(tree.args[1])
+        push!(inner,tree.args[2])
+        return inner
+    else
+        return vec([tree.args[1],tree.args[2]])
+    end
+end
+
+function buildyggexpr(inputexpr)
+    blockargs = Vector()
+    if inputexpr.head == Symbol("=")
+        tree = inputexpr.args[1]
+        indices = reftreetovec(tree)
+        blockargs = Vector()
+
+        for i in 2:length(indices)
+            varname = "$(indices[i])$i"
+            prevvarname = i == 2 ? indices[i-1] : "$(indices[i-1])$(i-1)"
+            prevsymbol = i==2 ? esc(Symbol(prevvarname)) : Symbol(prevvarname)
+            if i == length(indices)
+                myexpr = :($(prevsymbol)=set($(prevsymbol),
+                    $(indices[i]),$(esc(inputexpr.args[2]))))
+                push!(blockargs,myexpr)
+            else
+                myexpr = :($(Symbol(varname))=get($(prevsymbol),
+                    $(indices[i])))
+                push!(blockargs,myexpr)
+            end
+        end
+
+        for i in (length(indices)-1):-1:2
+            varname = "$(indices[i])$i"
+            prevvarname = i == 2 ? indices[i-1] : "$(indices[i-1])$(i-1)"
+            prevsymbol = i==2 ? esc(Symbol(prevvarname)) : Symbol(prevvarname)
+            myexpr = :($(prevsymbol)=set($(prevsymbol),
+                $(indices[i]),$(Symbol(varname))))
+            push!(blockargs,myexpr)
+        end
+        
+    end
+
+    if inputexpr.head == Symbol("ref")
+        tree = inputexpr
+        indices = reftreetovec(tree)
+    
+        for i in 2:length(indices)
+            varname = "$(indices[i])$i"
+            prevvarname = i == 2 ? indices[i-1] : "$(indices[i-1])$(i-1)"
+            prevsymbol = i==2 ? esc(Symbol(prevvarname)) : Symbol(prevvarname)
+            myexpr = :($(Symbol(varname))=get($(prevsymbol),
+                $(indices[i])))
+            push!(blockargs,myexpr)
+        end
+    end
+    return Expr(:block,blockargs...)
+end
+
+macro ygg(inputexpr)
+    return buildyggexpr(inputexpr)
 end
 
 struct YggdrasilNode
@@ -34,6 +93,36 @@ function set(node::YggdrasilNode,key::Any,value::Any)::YggdrasilNode
     )
 end
 
-export YggdrasilNode,get,set,@set
+function printnode(io::IO,node::YggdrasilNode;depth=0,visited=Set())
+    padding = (*)([' ' for i in 1:depth+1]...)
+
+    if isempty(visited)
+        if isnothing(node.key)
+            print(io,padding)
+            print(io,"[Root]")
+        end
+    end
+
+    if !isnothing(node.key) && !(node.key in visited)
+        print(io,padding)
+        print(io,node.key,"=>")
+        if typeof(node.value) == YggdrasilNode
+            println()
+            printnode(io,node.value,depth=depth+1)
+        else
+            print(io,node.value)
+            println(io)
+        end
+    end
+    if !(isnothing(node.tail))
+        printnode(io,node.tail,depth=depth,visited=push!(visited, node.key))
+    end
+end
+
+#Base.show(io::IO, node::YggdrasilNode) = printnode(io,node)
+# this is used to show values in the REPL and when using IJulia
+Base.show(io::IO, m::MIME"text/plain", node::YggdrasilNode) = printnode(io,node)
+
+export YggdrasilNode,get,set,@ygg
 
 end # module
